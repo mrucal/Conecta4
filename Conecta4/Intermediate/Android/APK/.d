d4667bd4fa1793c40b55f952f5ca8fffe49d7b9c
mrucal <rucalgr@gmail.com>
2019-07-14 17:41:34 +0200

    remove

--- Conecta4/Intermediate/Android/APK/.d
+++
@@ -1,6 +0,0 @@
-D:\Documents\Unreal\C4IATontaLimpio\Intermediate\Android\APK\gen\com\android\vending\licensing\ILicensingService.java : \
-  D:\Documents\Unreal\C4IATontaLimpio\Intermediate\Android\APK\src\com\android\vending\licensing\ILicensingService.aidl \
-  D:\Documents\Unreal\C4IATontaLimpio\Intermediate\Android\APK\src\com\android\vending\licensing\ILicenseResultListener.aidl
-
-D:\Documents\Unreal\C4IATontaLimpio\Intermediate\Android\APK\src\com\android\vending\licensing\ILicensingService.aidl :
-D:\Documents\Unreal\C4IATontaLimpio\Intermediate\Android\APK\src\com\android\vending\licensing\ILicenseResultListener.aidl :
--- Conecta4/Intermediate/Android/APK/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\Documents\Unreal\C4IATontaLimpio\Intermediate\Android\APK\bin\AndroidManifest.xml : \
-D:\Documents\Unreal\C4IATontaLimpio\Intermediate\Android\APK\AndroidManifest.xml \
